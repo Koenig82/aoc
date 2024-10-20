af6b05201cc4574ef3b700a1f8ef196e9e7c5579
Koenig82 <konigsson1982@gmail.com>
2024-10-20 22:15:56 +0200

    timing to nano

--- src/aoc.h
+++ src/aoc.h
@@ -28,14 +28,14 @@ class Aoc {
         auto start1 = std::chrono::high_resolution_clock::now();
         run1();
         auto end1 = std::chrono::high_resolution_clock::now();
-        auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1);
-        std::cout << "star 1 took " << duration1.count() << " milliseconds\n";
+        auto duration1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - start1);
+        std::cout << "star 1 took " << duration1.count() << " nanoseconds\n";
 
         auto start2 = std::chrono::high_resolution_clock::now();
         run2();
         auto end2 = std::chrono::high_resolution_clock::now();
-        auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2);
-        std::cout << "star 2 took " << duration2.count() << " milliseconds\n";
+        auto duration2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - start2);
+        std::cout << "star 2 took " << duration2.count() << " nanoseconds\n";
         return 0;
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 
 int main() {
-    Y2023d04 y2018d01;
+    Y2023d06 y2018d01;
     y2018d01.Aoc::start();
 
     return 0;
