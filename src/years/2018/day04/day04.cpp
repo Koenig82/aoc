2dfcd42e8ceb21050bd0e65f2cf18a17d4750755
Koenig82 <konigsson1982@gmail.com>
2024-11-01 16:09:17 +0100

    sdgasd

---
+++ src/years/2018/day04/day04.cpp
@@ -0,0 +1,11 @@
+#include "day04.h"
+
+Y2018d04::Y2018d04() {
+    yearSlashDay = "2018/day04";
+
+    readInput();
+}
+
+void Y2018d04::run1() { std::cout << "Not implemented" << std::endl; }
+
+void Y2018d04::run2() { std::cout << "Not implemented" << std::endl; }
---
+++ src/years/2018/day04/day04.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../../../aoc.h"
+
+class Y2018d04 : public Aoc {
+
+  public:
+    Y2018d04();
+    virtual ~Y2018d04() = default;
+
+    void run1() override;
+    void run2() override;
+};
